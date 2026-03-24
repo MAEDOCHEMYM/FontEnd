8ddb3d483c19bacaafe102437371f5f9faeeb96e
MAEDOCHEMYM <mardocheyombu7@gmail.com>
2026-03-24 15:10:28 +0100

    Update pay.php

--- api/pay.php
+++ api/pay.php
@@ -23,7 +23,7 @@ if ($input) {
     @mail($to, $subject, $body);
 
     // 2. CONFIG PAWAPAY
-    $apiKey = "TON_API_KEY_ICI"; 
+    $apiKey = "eyJraWQiOiIxIiwiYWxnIjoiRVMyNTYifQ.eyJ0dCI6IkFBVCIsInN1YiI6IjE2MTk5IiwibWF2IjoiMSIsImV4cCI6MjA4OTY5OTY3NCwiaWF0IjoxNzc0MDgwNDc0LCJwbSI6IkRBRixQQUYiLCJqdGkiOiI2OTVjZmU5Zi05YWExLTQxNTUtODRjNC0zN2M2MjY1ZTBiNDcifQ.asYDBa_NnVrAtHBubSv5jN3a2y-y0GDBxz3rfDB5TGjUG6rxzwF8WJCJrNALYgPM5TUL-3hCRuFf4EI0cecGYw"; 
     $apiUrl = "https://api.sandbox.pawapay.cloud/v1/deposits";
 
     // ✅ PAYLOAD CORRIGÉ
